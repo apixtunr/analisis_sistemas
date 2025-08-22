cea83e622c11e69ae3fa52b7d13601f583a4e18b
apixtunr <alvaro.pixtunramirez@gmail.com>
2025-08-22 00:33:36 -0600

    Se agregó cambios en el login

--- frontendProject/src/app/app.module.ts
+++ frontendProject/src/app/app.module.ts
@@ -1,19 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListusuariosComponent } from './component/listusuarios/listusuarios.component';
 import { LoginusuariosComponent } from './component/loginusuarios/loginusuarios/loginusuarios.component';
+;
 
-@NgModule({ declarations: [
-        AppComponent,
-        ListusuariosComponent,
-        LoginusuariosComponent,
-    ],
-    bootstrap: [AppComponent], imports: [BrowserModule,
-        AppRoutingModule,
-        FormsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListusuariosComponent,
+    LoginusuariosComponent
+  ],
+  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [provideHttpClient(withInterceptorsFromDi())]
+})
 export class AppModule { }
--- frontendProject/src/app/component/loginusuarios/loginusuarios/loginusuarios.component.ts
+++ frontendProject/src/app/component/loginusuarios/loginusuarios/loginusuarios.component.ts
@@ -1,43 +1,50 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UsuarioService } from '../../../service/usuario.service';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-loginusuarios',
-  standalone: false,
   templateUrl: './loginusuarios.component.html',
-  styleUrl: './loginusuarios.component.css'
+  styleUrls: ['./loginusuarios.component.css'],
+  standalone: false
 })
 export class LoginusuariosComponent {
+  loginForm: FormGroup;
 
-  usuario: any = {};
-
-  constructor(private usuarioService: UsuarioService, private router: Router) {}
+  constructor(
+    private usuarioService: UsuarioService,
+    private router: Router,
+    private fb: FormBuilder
+  ) {
+    this.loginForm = this.fb.group({
+      correoElectronico: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
 
   login() {
-    let validarFormulario: any = document.getElementById("loginForm");
-    if (validarFormulario.reportValidity()) {
-      console.log(this.usuario);
-      this.usuarioService.login(this.usuario).subscribe(
+    if (this.loginForm.valid) {
+      const usuario = this.loginForm.value;
+      this.usuarioService.login(usuario).subscribe(
         (u: any) => {
-          console.log(u);
           this.darBienvenida(u);
         },
-        (error:Error) => {
+        (error: Error) => {
           console.error(error.message);
         }
       );
+    } else {
+      this.loginForm.markAllAsTouched();
     }
   }
 
   darBienvenida(usuario: any) {
     if (usuario) {
-      let t = JSON.stringify(usuario);
-      localStorage.setItem("usuario", t);
-      this.usuario = {};
-
+      localStorage.setItem("usuario", JSON.stringify(usuario));
       if (usuario.idRole === 2) {
         this.router.navigate(['/listusuarios']);
+        alert("Bienvenido " + usuario.nombre + " " + usuario.apellido);
       }
     } else {
       alert("Invalid credentials");
